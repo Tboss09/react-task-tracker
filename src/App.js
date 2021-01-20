43dc7774dff273c4cd1dc5bd6f723c5a81126564
Akosile Tayo <akosilety9@gmail.com>
2021-01-20 11:10:32 +0100

    Playing around

--- src/App.js
+++ src/App.js
@@ -2,9 +2,12 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const name = "Tayo"
+  let x = false;
   return (
-    <div className="App">
+    <div className="container">
       <h1>Hello From React</h1>
+      <h1>Hello {x ? "Tayo" : "Akosile"}</h1>
     </div>
   );
 }
