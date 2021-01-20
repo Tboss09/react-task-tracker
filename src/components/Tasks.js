c550ed4786237343e357eb7d5c42e9a0ad02fa45
Akosile Tayo <akosilety9@gmail.com>
2021-01-20 23:38:05 +0100

    Tasks .js just addedd

--- src/components/Tasks.js
+++ src/components/Tasks.js
@@ -1,9 +1,26 @@
 import React from 'react'
-const tasks = [{
-    id: 1
-text:"Dctors"
+const tasks = [
+    {
+    id: 1,
+    text: "Doctors Apartment",
+    day: "Feb 5th at 2:30pm",
+    reminder:true
 
 },
+    {
+    id: 2,
+    text: "Meeting at School",
+    day: "Feb 6th at 1:30pm",
+    reminder:true
+
+},
+    {
+    id: 3,
+    text: "Food Shopping",
+    day: "Feb 5th at 2:30pm",
+    reminder:true
+
+}
 
 
 ]
