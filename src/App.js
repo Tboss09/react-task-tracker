dafe466454799cb381027596e7a151a8eced7ae3
Akosile Tayo <akosilety9@gmail.com>
2021-01-20 14:57:48 +0100

    Started again

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import Header from './components/Header';
+// // import { useState } from 'react';
+// import Header from './components/Header';
+// import Tasks from './components/Tasks';
 
 
+// function App() {
+//   // 
+//   return (
+//     <div className="container">
+//       <Header title="Task Runner" />
+//       <Tasks />
+//     </div >
+//   );
+// }
+
+// export default App;
 function App() {
   return (
-    <div className="container">
-      <Header title ="Task Runner"/>
-    </div >
-  );
+    <div className="App">
+      <h1>Hello From React</h1>
+    </div>
+  )
 }
 
 export default App;
