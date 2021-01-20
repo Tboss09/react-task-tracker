74c0b002552dc696e7139fe7757fabf610634e38
Akosile Tayo <akosilety9@gmail.com>
2021-01-20 11:45:12 +0100

    Added Styles to index.css

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,27 @@
 import React from 'react'
-const Header = (props) => {
+const Header = ({ title }) => {
     return (
 
+        // When using inline styles in jsx, put it in braces*2 -->{{}}
+        //Or create an object,input the style then replace it with just one brace   -->{{}}
+
         <header>
-            <h1>{props.title}</h1>
+            <h1>{title}</h1>
         </header>
     )
 }
+
+// You can set default Props like this
+// Header.defaultProps = {
+//     title: "Task Runner"
+// }
+
+// // css in Js
+// const headingStyle = {
+//     color: "red",
+//     background: "black",
+//     fontFamily: "poppins",
+
+// };
+
 export default Header;
